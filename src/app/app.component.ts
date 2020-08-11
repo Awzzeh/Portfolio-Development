8c0041f7dc4f0c402bd27fcaa6713f30e1d362fd
Awzzeh <awzzeh.p@gmail.com>
2020-08-11 10:00:36 -0700

    Setting up timer
    trying to figure out how to create a dynamic timer on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, NgZone, Renderer2 } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Portfolio';
+  datetime = null,
+        date = null;
+
+update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+  @ViewChild('live')
+  public liveTimer: ElementRef;
+
+  constructor(private zone: NgZone, private renderer: Renderer2) {
+    this.zone.runOutsideAngular(() => {
+      setInterval(() => {
+        this.renderer.setProperty(
+          this.liveTimer.nativeElement,
+          'textContent',
+          this.getLiveTime()
+        );
+      }, 1000);
+    });
+  }
 }
