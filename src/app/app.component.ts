03229ffb244a34dbadfcfe910c7399271dfb774c
Awzzeh <awzzeh.p@gmail.com>
2020-08-13 08:55:17 -0700

    Live Dynamic Clock Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, ViewChild, ElementRef, NgZone, Renderer2 } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  NgZone,
+  Renderer2,
+} from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
@@ -7,26 +13,19 @@ import * as moment from 'moment';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  datetime = null,
-        date = null;
-
-update = function () {
-    date = moment(new Date())
-    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
-};
 
   @ViewChild('live')
-  public liveTimer: ElementRef;
+  public live: ElementRef;
 
   constructor(private zone: NgZone, private renderer: Renderer2) {
     this.zone.runOutsideAngular(() => {
       setInterval(() => {
         this.renderer.setProperty(
-          this.liveTimer.nativeElement,
+          this.live.nativeElement,
           'textContent',
-          this.getLiveTime()
+          moment().format('h:mm:ss A')
         );
-      }, 1000);
+      }, 500);
     });
   }
 }
